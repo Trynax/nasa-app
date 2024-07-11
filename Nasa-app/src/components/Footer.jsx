0fe992cdab7a9085b10f319850afc8e0ccf4ff20
Trynax <kandoso111@gmail.com>
2024-07-11 18:04:44 +0100

    save dimages

---
+++ Nasa-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div>
+            <h2>The Brutal Martin Landscape</h2>
+            <h1>APOD PROJECT</h1>
+        </div>
+        <button>
+            
+        </button>
+    </footer>
+  )
+}
